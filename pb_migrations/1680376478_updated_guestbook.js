9c435352918be039410c59f464c2e58d2f099191
vlausargs <vlaus.mi@gmail.com>
2023-04-02 02:22:16 +0700

    db

---
+++ pb_migrations/1680376478_updated_guestbook.js
@@ -0,0 +1,71 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("kr0xaqpd7rcgg95")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jfchv77j",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zse5pcal",
+    "name": "comment",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("kr0xaqpd7rcgg95")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jfchv77j",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zse5pcal",
+    "name": "comment",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
